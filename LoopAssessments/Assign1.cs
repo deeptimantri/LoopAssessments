46f1e4f9864672446d2b48683de7b1826ced4746
dipti.mantri@gmail.com <dipti.mantri@gmail.com>
2023-09-02 00:03:35 -0700

    commit

---
+++ LoopAssessments/Assign1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopAssignments
+{
+    class Assign1
+    {
+        static void Main(string[] args)
+        {
+            int i = 121;
+
+            do
+            {
+                if(i % 2 == 0)
+              Console.WriteLine(i);
+                i++;
+            } while (i <= 229);
+
+        }
+    }
+}
---
+++ LoopAssessments/Assign1_1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopAssignments
+{
+     class Assign1_1
+    {
+        static void Main(string[] args)
+        {
+            int i, N;
+            Console.WriteLine("Enter N:");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= N; i++)
+            {
+                Console.WriteLine( i * 10);
+            }
+        }
+    }
+}
